9fdd9a4a9d7c0c5d67f1294953e640ce43dd2292
Burak <avniburakcitlak@gmail.com>
2022-07-16 16:36:03 +0300

    add working clear button

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 window.onload=function(){
     const grid=document.querySelector('.sketch');
+    const clean=document.querySelector('#clear');
+    clean.addEventListener('click',()=>clear())
     
 
     for (let i = 0; i < 16*16; i++) {
         const dot=document.createElement('div');
         dot.classList.add('grid');
+        dot.addEventListener('dragstart', (e) => {
+            e.preventDefault()
+          })
+          
         dot.addEventListener("mousedown",(e) =>{e.currentTarget.style.background='black'})
         dot.addEventListener('mouseover',(e)=>{
             if(e.buttons == 1 || e.buttons == 3){
@@ -16,6 +22,11 @@ window.onload=function(){
         grid.appendChild(dot);
     }
 }
-
+function clear(){
+    const canvas=document.querySelectorAll('.grid');
+    canvas.forEach((gr)=>{
+        gr.style.background='white';
+    })
+}
 
 
